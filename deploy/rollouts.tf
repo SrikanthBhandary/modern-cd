81b46f7ce48b414e5fff4de09120dc871df4b058
srikanth <srikanth@Akshitas-MacBook-Air.local>
2026-07-08 12:58:54 +0200

    added the plugins

--- deploy/rollouts.tf
+++ deploy/rollouts.tf
@@ -34,3 +34,56 @@ resource "helm_release" "argo_rollouts_spoke2" {
     file("${path.module}/../config/dev/argo_values.yaml")
   ]
 }
+
+
+resource "kubernetes_cluster_role_v1" "argo_rollouts_gatewayapi" {
+  provider = kubernetes.hub
+
+  metadata {
+    name = "argo-rollouts-gatewayapi"
+  }
+
+  rule {
+    api_groups = ["gateway.networking.k8s.io"]
+    resources  = ["httproutes"]
+
+    verbs = [
+      "get",
+      "list",
+      "watch",
+      "update",
+      "patch",
+    ]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["services"]
+
+    verbs = [
+      "get",
+      "list",
+      "watch",
+    ]
+  }
+}
+
+resource "kubernetes_cluster_role_binding_v1" "argo_rollouts_gatewayapi" {
+  provider = kubernetes.hub
+
+  metadata {
+    name = "argo-rollouts-gatewayapi"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role_v1.argo_rollouts_gatewayapi.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "argo-rollouts"
+    namespace = "argo-rollouts"
+  }
+}
